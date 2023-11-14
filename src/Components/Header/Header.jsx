53bf4931021eb4507f6c896d27f72c213c660b4b
AkhiSharmin <akhisharmin100@gmail.com>
2023-11-14 17:52:18 +0600

    some style add

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,13 +3,15 @@ import logo from "../../assets/logo/logo.jpg";
 
 const Header = () => {
   return (
-    <div className="max-w-7xl p-8">
-      <img src={logo} alt="" />
-      <a href="/shop">Shop</a>
-      <a href="/orders">Orders</a>
-      <a href="/inventory">Inventory</a>
-      <a href="/login">Login</a>
-    </div>
+    <nav className="flex h-20 pl-24 pr-24 justify-between bg-slate-700 items-center">
+      <img className="h-14" src={logo} alt="" />
+      <div className="text-white font-semibold">
+        <a href="/shop">Shop</a>
+        <a href="/orders">Orders</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
   );
 };
 
