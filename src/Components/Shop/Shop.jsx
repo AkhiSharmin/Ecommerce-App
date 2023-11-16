b05f06818cf649c44d982b668f004bdfcf8383d9
AkhiSharmin <akhisharmin100@gmail.com>
2023-11-16 19:03:43 +0600

    some update quantity functionality

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -33,16 +33,26 @@ const Shop = () => {
         //step-4 add the addedProduct to the saved cart
         savedCart.push(addedProduct);
       }
-      //   else {
-      //     console.log(`Product with id ${id} not found in the products array.`);
-      //   }
     }
     //step-5 set the cart
     setCart(savedCart);
   }, [products]);
 
   const handelAddToCart = (product) => {
-    const newCart = [...cart, product];
+    let newCart = [];
+    // const newCart = [...cart, product];
+    //if product doesn't exit in the cart , then set quantity = 1
+    //if exist update quantity by 1
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    }
+
     setCart(newCart);
     addToDb(product.id);
   };
