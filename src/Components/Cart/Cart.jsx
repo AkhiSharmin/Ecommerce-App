e157197b98b8831857e53e67f132cab366830816
AkhiSharmin <akhisharmin100@gmail.com>
2023-11-16 17:28:37 +0600

    calculateted all product quantity

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,9 +4,17 @@ const Cart = ({ cart }) => {
   let totalPrice = 0;
   //   calculate shipping
   let totalShipping = 0;
+  //calculate quantity
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    if (product.quantity === 0) {
+      product.quantity = 1;
+    }
+    // product.quantity = product.quantity || 1;
+
+    totalPrice = totalPrice + product.price + product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 
   //calculate tax.. total tk r 7% hobe tax
@@ -18,8 +26,8 @@ const Cart = ({ cart }) => {
   return (
     <div className="bg-slate-100 p-8 text-[17px] sticky top-0">
       <h3 className="pb-2 font-bold text-2xl">Oder summary</h3>
-      <p className="pb-2">Selected Items: {cart.length}</p>
-      <p className="pb-2">Total Price: {totalPrice}$</p>
+      <p className="pb-2">Selected Items: {quantity}</p>
+      <p className="pb-2">Total Price: {totalPrice.toFixed(2)}$</p>
       <p className="pb-2">Total Shipping: {totalShipping}$</p>
       <p className="pb-2">Tax: {totalTax.toFixed(2)}$</p>
       <p>Grand Total: {grandTotal.toFixed(2)}$</p>
